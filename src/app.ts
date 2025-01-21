9e9078cb67219f3fcefbf591acd0ed1731dc9e73
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2025-01-20 21:55:24 -0300

    criação das rotas da api

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,46 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({message: "Hello World!"})
+  res.json({message: "Hello World!"});
+});
+
+app.post('/auth/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send();
+});
+
+app.post('/partners', (req, res) => {
+  const { name, email, password, company_name } = req.body;
+});
+
+app.post('/customers', (req, res) => {
+  const { name, email, password, address, telephone } = req.body;
+});
+
+// Criação do evento pelo parceiro
+app.post('/partners/events', (req, res) => {
+  const { name, description, date, location } = req.body;
+});
+
+// Listagem de evento do parceiro
+app.get('/partners/events/:eventId', (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
+});
+
+app.post('/events', (req, res) => {
+  const { name, description, date, location } = req.body;
+});
+
+app.get('/events/:eventId', (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
 });
 
 app.listen(3000, () => {
